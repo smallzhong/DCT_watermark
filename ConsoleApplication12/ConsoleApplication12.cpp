cb14cecc1740a48aa37a5cc39879bd72a8d3702b
小钟 <55563027+smallzhong@users.noreply.github.com>
2022-06-07 08:47:55 +0800

    配置环境

--- ConsoleApplication12/ConsoleApplication12.cpp
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -1,9 +1,22 @@
-﻿#include <opencv2/opencv.hpp>
-#include <iostream>
+﻿#include <iostream>
+#include "head.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
+
+
+using namespace cv;
+
+void init()
+{
+	cv::utils::logging::setLogLevel(utils::logging::LOG_LEVEL_SILENT);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	init();
+	Mat backImg = imread("lena1.png");
+	imshow("lena1.png", backImg);
+	waitKey(0);
 }
 
 
---
+++ ConsoleApplication12/head.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <bitset>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+//extern "C" int getopt(int argc, char* const* argv, const char* optstring);
+
+typedef union _LARGE_INTEGER {
+    struct {
+        unsigned int LowPart;
+        int HighPart;
+    } DUMMYSTRUCTNAME;
+    struct {
+        unsigned int LowPart;
+        int HighPart;
+    } u;
+    long long QuadPart;
+} LARGE_INTEGER;
+
+#define EXIT_ERROR(x)                                 \
+	do                                                \
+	{                                                 \
+		cout << "error in file " << __FILE__ << " in line " << __LINE__ << endl; \
+		cout << x;                                    \
+		getchar();                                    \
+		exit(EXIT_FAILURE);                           \
+	} while (0)
